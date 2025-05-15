bb02864190d056604f496a83a90e2daaecba16ab
Xucong Zhan <xzhan@connect.ust.hk>
2025-05-16 00:35:16 +0800

    feat: clean up counter cmp with icon cmp and semantic css classes

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -6,19 +6,20 @@ import {
   Output,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { StopIconComponent } from './stop-icon.component';
 
 @Component({
   selector: 'app-counter',
-  imports: [FormsModule],
+  imports: [FormsModule, StopIconComponent],
   template: `
-    <div class="flex flex-col space-y-2 p-3 rounded-md border-2 border-primary">
-      <span class="italic font-serif">{{ hello }}</span>
-      <div class="flex items-center-safe space-x-4">
+    <div class="counter-container">
+      <span class="greeting">{{ hello }}</span>
+      <div class="counter-row">
         <button (click)="decrement()" class="btn btn-primary">-</button>
         @if (!isEditMode) {
           <span (dblclick)="toggleEditMode()">
             Current Count:
-            <span class="font-bold text-lg text-warning">{{ _count }}</span>
+            <span class="counter-value">{{ _count }}</span>
           </span>
         } @else {
           <input
@@ -30,22 +31,12 @@ import { FormsModule } from '@angular/forms';
         }
         <button (click)="increment()" class="btn btn-primary">+</button>
       </div>
-      <button (click)="reset()" class="mt-2 self-end btn btn-circle btn-error">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="size-6"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
-            clip-rule="evenodd"
-          />
-        </svg>
+      <button (click)="reset()" class="reset-btn">
+        <app-icon-stop />
       </button>
     </div>
   `,
+  styleUrl:'./counter.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent {
